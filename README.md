42617a253da20ec1124028dae66f494d8baf370e
Julien Lemaître <julien@lemaitre-creation.fr>
2017-08-22 09:35:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# D3 Force Directed Graph of shared borders between countries
+
+Show common borders between all countries of the world.
+
+This work is part of the Free Code Camp data visualization certification.
+
+# Techno used
+
+* D3
+* React
+* React-Faux-DOM
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Shared borders - D3 Forced Directed Graph</h1>
+        </div>
+        <div className="App-body">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
